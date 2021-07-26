7aecd7f5b1ea76539bd10909e291fb4b226b8d61
krish <krishnachaitanya.tkc@gmail.com>
2021-07-26 11:40:33 +0530

    BaseTest commit

--- Venkatchaitusampleproject/src/main/java/com/org/TestBase/TestBase.java
+++ Venkatchaitusampleproject/src/main/java/com/org/TestBase/TestBase.java
@@ -1,5 +1,15 @@
 package com.org.TestBase;
 
-public class TestBase {
+import org.openqa.selenium.WebDriver;
 
+public class TestBase {
+	private WebDriver driver;
+	
+	public void setUp() {
+		
+	}
+	
+	public void quit() {
+		driver.quit();
+	}
 }
