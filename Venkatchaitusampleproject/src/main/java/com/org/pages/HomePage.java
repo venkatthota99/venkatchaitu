8c1ff1e92a010a4f96a0671431db68e9a1f60c84
thota <vthota.ca@gmail.com>
2021-07-27 11:34:58 +0530

    Config,util

---
+++ Venkatchaitusampleproject/src/main/java/com/org/pages/HomePage.java
@@ -0,0 +1,7 @@
+package com.org.pages;
+
+public class HomePage {
+	
+	
+
+}
---
+++ Venkatchaitusampleproject/src/main/java/com/org/util/TestUtil.java
@@ -0,0 +1,8 @@
+package com.org.util;
+
+public class TestUtil {
+	
+	public static long page_load_timeout =20;
+	public static long implicit_wait = 10;
+
+}
