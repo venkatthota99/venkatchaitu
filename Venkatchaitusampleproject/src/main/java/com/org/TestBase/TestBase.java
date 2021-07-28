6da29d80575c999b9915d4fc901036d368e307f9
thota <vthota.ca@gmail.com>
2021-07-28 08:12:33 +0530

    First run - checkbox, selectdropdown

--- Venkatchaitusampleproject/src/main/java/com/org/TestBase/TestBase.java
+++ Venkatchaitusampleproject/src/main/java/com/org/TestBase/TestBase.java
@@ -6,13 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 import com.org.util.TestUtil;
 
@@ -20,29 +16,30 @@ import com.org.util.TestUtil;
 public class TestBase {
 	
 
-	static WebDriver driver;     // static variables can be called directly or using class name eg: prop or testbase.prop
-	static Properties prop;      // static means method or variable associated with class and not with object
+	public static WebDriver driver;     // static variables can be called directly or using class name eg: prop or testbase.prop
+	public static Properties prop;      // static means method or variable associated with class and not with object
 	
 	
-	public TestBase() throws IOException {  // public, private, protected are access modifiers
+	public TestBase()  {  // public, private, protected are access modifiers
 		
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
-					System.getProperty("/Users/Dell/git/repository/Venkatchaitusampleproject/src/main/java/com/org/config/config.properties"));
+					("/Users/Dell/git/repository/Venkatchaitusampleproject/src/main/java/com/org/config/config.properties"));
 			prop.load(ip);
 		
 		}catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}catch(IOException e) {
+		}
+		catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 		
 		public static void Initialization() {  // void means this method has no return value
-			String browsername = System.getProperty("browser");
+			String browsername = prop.getProperty("browser");
 			if(browsername.equals("chrome")) {
-				System.setProperty("webdriver.chrome.driver","E:/Selenium 2020/chromedriver_win32/chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver","E:/Selenium 2020/chromedriver/chromedriver.exe");
 				driver = new ChromeDriver();
 			}
 			driver.manage().window().maximize();
--- Venkatchaitusampleproject/src/main/java/com/org/pages/InputForms.java
+++ Venkatchaitusampleproject/src/main/java/com/org/pages/InputForms.java
@@ -1,5 +1,87 @@
 package com.org.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.org.TestBase.TestBase;
 
-public class InputForms {
 
+public class InputForms extends TestBase {
+	
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@FindBy (linkText ="Demo Website!")
+	private WebElement DemoWebsite;
+	
+	@FindBy (xpath ="//*[@id='at-cv-lightbox-close']")
+	private WebElement closelightwindow;
+	
+	@FindBy (linkText ="Input Forms")
+	private WebElement InputForms;
+	
+	@FindBy(linkText ="Checkbox Demo")
+    private WebElement checkboxdemo;
+	
+	@FindBy(xpath="//*[@id='isAgeSelected']")
+	private WebElement checkbox;
+	
+	@FindBy(xpath="//*[@id='txtAge']")
+	private WebElement checkboxmsg;
+	
+	@FindBy(linkText ="Select Dropdown List")
+    private WebElement selectdropdownlist;
+	
+	@FindBy(id="select-demo")
+	private WebElement selectdropdown;
+	
+	@FindBy(className="selected-value")
+	private WebElement selectedmessage;
+	
+		
+	//Initializing
+	public InputForms (WebDriver driver) {
+		  this.driver = driver;
+		  this.wait= new WebDriverWait(driver, 30);
+		  PageFactory.initElements(driver, this);
+	}
+	
+	//Actions
+	public void Gotodemowebsite() {
+		this.DemoWebsite.click();
+	}
+	
+	public void closelightwindow() {
+		this.closelightwindow.click();
+	}
+	
+	
+	public void Gotocheckbox() {
+		this.InputForms.click();
+		this.checkboxdemo.click();
+	}
+	
+	public void selectcheckbox() {
+		this.checkbox.click();
+	}
+	
+	public String checkboxmessage() {
+		  return this.checkboxmsg.getText();
+	}
+	
+	public void selectdropdownlist() {
+		this.InputForms.click();
+		this.selectdropdownlist.click();
+	}
+	
+	public void selectsunday() {
+		Select select = new Select(this.selectdropdown);
+		select.selectByVisibleText("Sunday");
+	}
+		
+	public String selectedmessage() {
+		  return this.selectedmessage.getText();
+	}
 }
